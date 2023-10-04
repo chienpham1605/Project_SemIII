f0713afe36509d600ce651555e608c02cb1621d7
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2023-10-04 08:53:23 +0700

    git add

---
+++ src/Client/Client/Controllers/ClientController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ParselCreate ()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MoneyOrder()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Statistic()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
