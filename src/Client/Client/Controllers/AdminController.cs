8a81f2f5d5f16b8dcbf61ca3ae659417986aac10
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2023-10-06 23:46:54 +0700

    git add.

---
+++ src/Client/Client/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MoneyOrderManage()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ParcelManage()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EditMoneyOrder()
+        {
+            return View();
+        }
+        public IActionResult EditParcel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult UserManage()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+    
+    }
